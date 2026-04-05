cbb2211893e1d26e4396cf24eb0ea50b4bb3a15e
garcia1701 <garcia1701@gmail.com>
2026-04-05 17:34:46 +0200

    Delete contador_visitas.js

--- contador_visitas.js
+++
@@ -1,8 +0,0 @@
-/* ============================
-   CONTADOR DE VISITAS DISCRETO
-   Registra visitas sin mostrar nada
-============================ */
-
-fetch("https://api.countapi.xyz/hit/pegueraytaurina/visitas")
-  .then(r => r.json())
-  .then(d => console.log("Visitas totales:", d.value));
